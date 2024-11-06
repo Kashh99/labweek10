1cabfcc49c3026a02ea66975a19328b5a4352fc0
kash99 <kashyapmavani@yahoo.in>
2024-11-05 20:56:35 -0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FormComponent from "./components/FormComponent";
+import SubmittedData from "./components/SubmittedData";
 
 function App() {
+  const [submittedData, setSubmittedData] = useState(null);
+
+  const handleFormSubmit = (data) => {
+    setSubmittedData(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Entry Form</h1>
+      <FormComponent onSubmit={handleFormSubmit} />
+      {submittedData && <SubmittedData data={submittedData} />}
     </div>
   );
 }
